fca4346453e526df0479ea17c750c0b47937a945
Siradz Sahiddin <141974098+iaa-ai@users.noreply.github.com>
2024-10-29 23:02:06 +0800

    refractor admin.py

--- projectsite/studentorg/admin.py
+++ projectsite/studentorg/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+# 3
+from .models import College, Program, Organization, Student, OrgMember
 
 # Register your models here.
 #2
@@ -8,4 +10,23 @@ admin.site.register(College)
 admin.site.register(Program)
 admin.site.register(Organization)
 admin.site.register(Student)
+# 4
+# @admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ("student_id", "lastname",
+                    "firstname", "middlename", "program")
+    search_fields = ("lastname", "firstname",)
+
 admin.site.register(OrgMember)
+# 5
+# @admin.register(OrgMember)
+class OrgMemberAdmin(admin.ModelAdmin):
+    list_display = ("student", "get_member_program", "organization", "date_joined",)
+    search_fields = ("student__lastname", "student__firstname",)
+
+    def get_member_program(self, obj):
+        try:
+            member = Student.objects.get(id=obj.student_id)
+            return member.program
+        except Student.DoesNotExist:
+            return None

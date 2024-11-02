2d97b2ffd10ebaffb5de6474d5436e02c780a70c
Siradz Sahiddin <141974098+iaa-ai@users.noreply.github.com>
2024-11-02 21:54:02 +0800

    list of organization

--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic.list import ListView
+from studentorg.models import Organization
+
+class HomePageView(ListView):
+    model = Organization
+    context_object_name = 'home'
+    template_name = 'home.html'
+
+class OrganizationList(ListView):
+    model = Organization
+    context_object_name = 'organization'
+    template_name = 'org_list.html'
+    paginated_by = 5

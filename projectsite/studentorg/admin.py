047d99ea15351d50cc54a41516295c70767a7f5a
Siradz Sahiddin <141974098+iaa-ai@users.noreply.github.com>
2024-11-04 23:10:25 +0800

    Student CRUD

--- projectsite/studentorg/admin.py
+++ projectsite/studentorg/admin.py
@@ -9,17 +9,15 @@ from .models import College, Program, Organization, Student, OrgMember
 admin.site.register(College)
 admin.site.register(Program)
 admin.site.register(Organization)
-admin.site.register(Student)
 # 4
-# @admin.register(Student)
+@admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
     list_display = ("student_id", "lastname",
                     "firstname", "middlename", "program")
     search_fields = ("lastname", "firstname",)
 
-admin.site.register(OrgMember)
 # 5
-# @admin.register(OrgMember)
+@admin.register(OrgMember)
 class OrgMemberAdmin(admin.ModelAdmin):
     list_display = ("student", "get_member_program", "organization", "date_joined",)
     search_fields = ("student__lastname", "student__firstname",)
--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -3,10 +3,16 @@ from django.shortcuts import render
 # Create your views here.
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from studentorg.models import Organization
-from studentorg.forms import OrganizationForm
+from studentorg.models import Organization, Student
+from studentorg.forms import OrganizationForm, StudentForm
 from django.urls import reverse_lazy
+from typing import Any
+from django.db.models.query import QuerySet
+from django.db.models import Q
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
 
+@method_decorator(login_required, name='dispatch')
 class HomePageView(ListView):
     model = Organization
     context_object_name = 'home'
@@ -17,6 +23,13 @@ class OrganizationList(ListView):
     context_object_name = 'organization'
     template_name = 'org_list.html'
     paginated_by = 5
+
+    def get_queryset(self, *args, **kwargs):
+        qs = super(OrganizationList, self).get_queryset(*args, **kwargs)
+        if self.request.GET.get("q") != None:
+            query = self.request.GET.get("q")
+            qs = qs.filter(Q(name__icontains=query)|Q(description__icontains=query))
+        return qs
 
 class OrganizationCreateView(CreateView):
     model = Organization
@@ -34,3 +47,34 @@ class OrganizationDeleteView(DeleteView):
     model = Organization
     template_name = 'org_del.html'
     success_url = reverse_lazy('organization-list')
+
+# Student
+class StudentList(ListView):
+    model = Student
+    context_object_name = 'student'
+    template_name = 'stud_list.html'
+    paginated_by = 5
+
+    def get_queryset(self, *args, **kwargs):
+        qs = super(StudentList, self).get_queryset(*args, **kwargs)
+        if self.request.GET.get("q") != None:
+            query = self.request.GET.get("q")
+            qs = qs.filter(Q(name__icontains=query)|Q(description__icontains=query))
+        return qs
+
+class StudentCreateView(CreateView):
+    model = Student
+    form_class = StudentForm
+    template_name = 'stud_add.html'
+    success_url = reverse_lazy('student-list')
+
+class StudentUpdateView(UpdateView):
+    model = Student
+    form_class = StudentForm
+    template_name = 'stud_edit.html'
+    success_url = reverse_lazy('student-list')
+
+class StudentDeleteView(DeleteView):
+    model = Student
+    template_name = 'stud_del.html'
+    success_url = reverse_lazy('student-list')

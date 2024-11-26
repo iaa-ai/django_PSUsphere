17e6fca567723f90d9ba02534deb90ae2dadbe4b
Siradz Sahiddin <141974098+iaa-ai@users.noreply.github.com>
2024-11-26 20:39:02 +0800

    add verbose name

--- projectsite/studentorg/models.py
+++ projectsite/studentorg/models.py
@@ -16,14 +16,14 @@ class College(BaseModel):
         return self.college_name #super().__str__()
     
 class Program(BaseModel):
-    prog_name = models.CharField(max_length=150)
+    prog_name = models.CharField(max_length=150, verbose_name="Program Name")
     college = models.ForeignKey(College, on_delete=models.CASCADE)
 
     def __str__(self): #-> str:
         return self.prog_name #super().__str__()
     
 class Organization(BaseModel):
-    name = models.CharField(max_length=250)
+    name = models.CharField(max_length=250, verbose_name="Organization Name")
     college = models.ForeignKey(College, null=True, blank=True, on_delete=models.CASCADE)
     description = models.CharField(max_length=500)
 
@@ -33,8 +33,8 @@ class Organization(BaseModel):
 class Student(BaseModel):
     student_id = models.CharField(max_length=15)
     lastname = models.CharField(max_length=25, verbose_name="Last Name")
-    firstname = models.CharField(max_length=25)
-    middlename = models.CharField(max_length=25, blank=True, null=True)
+    firstname = models.CharField(max_length=25, verbose_name="First Name")
+    middlename = models.CharField(max_length=25, blank=True, null=True, verbose_name="Middle Name")
     program = models.ForeignKey(Program, on_delete=models.CASCADE)
 
     def __str__(self): #-> str:
